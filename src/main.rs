8ce3f4809284b6ecf88793d4c8ac87bef3cd822a
EIG520 <72418797+EIG520@users.noreply.github.com>
2026-06-24 15:53:00 -0500

    removfe debug info

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 pub use scarlet::board::*;
 pub use scarlet::search::*;
-use std::env;
 
 pub fn main() {
-    env::set_var("RUST_BACKTRACE", "full");
-
     let mut uci: UciHandler = UciHandler::new();
 
     uci.uci();
